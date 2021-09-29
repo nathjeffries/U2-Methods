9828ebb41f469c6ef3f3ad06c69c56652f43024d
nathjeffries <jeffrnat000@warren.k12.in.us>
2021-09-29 12:26:30 -0400

    drippy

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class BasicCalculator {
 
     /* Create a calculator program with two text fields
@@ -8,4 +11,90 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
+   static JTextField inputField1, inputField2;
+   static JLabel resultLabel;
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Calculator");
+        JPanel panel = new JPanel();
+        JLabel inputLabel1 = new JLabel("Enter a number: ");
+        JLabel inputLabel2 = new JLabel("Enter a number: ");
+        inputField1 = new JTextField(10);
+        inputField2 = new JTextField(10);
+
+
+        JButton add = new JButton("+");
+        JButton subtract = new JButton("-");
+        JButton multiply = new JButton("X");
+        JButton divide = new JButton("/");
+
+        resultLabel = new JLabel("0.00");
+
+        window.setSize(250, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        add.addActionListener(new addListener());
+        subtract.addActionListener(new subtractListener());
+        multiply.addActionListener(new multiplyListener());
+        divide.addActionListener(new divideListener());
+
+
+        panel.add(inputLabel1);
+        panel.add(inputField1);
+        panel.add(inputLabel2);
+        panel.add(inputField2);
+        panel.add(resultLabel);
+        panel.add(add);
+        panel.add(subtract);
+        panel.add(multiply);
+        panel.add(divide);
+
+
+        window.add(panel);
+
+        window.setVisible(true);
+
+    }
+
+   private static class addListener implements ActionListener{
+
+       public void actionPerformed(ActionEvent actionEvent) {
+           double number1 = Double.parseDouble(inputField1.getText());
+           double number2 = Double.parseDouble(inputField2.getText());
+
+
+           resultLabel.setText(String.valueOf(number1 + number2));
+       }
+   }
+    private static class subtractListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent actionEvent) {
+            double number1 = Double.parseDouble(inputField1.getText());
+            double number2 = Double.parseDouble(inputField2.getText());
+
+
+            resultLabel.setText(String.valueOf(number1 - number2));
+        }
+    }
+    private static class multiplyListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent actionEvent) {
+            double number1 = Double.parseDouble(inputField1.getText());
+            double number2 = Double.parseDouble(inputField2.getText());
+
+
+            resultLabel.setText(String.valueOf(number1 * number2));
+        }
+    }
+    private static class divideListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent actionEvent) {
+            double number1 = Double.parseDouble(inputField1.getText());
+            double number2 = Double.parseDouble(inputField2.getText());
+
+
+            resultLabel.setText(String.valueOf(number1 / number2));
+        }
+    }
 }

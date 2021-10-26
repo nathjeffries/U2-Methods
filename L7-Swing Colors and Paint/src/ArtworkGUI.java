c5b3b5a739a613c53d9a62c42b42500f94f764e9
nathjeffries <jeffrnat000@warren.k12.in.us>
2021-10-26 13:12:21 -0400

    drip shield

--- L7-Swing Colors and Paint/src/ArtworkGUI.java
+++ L7-Swing Colors and Paint/src/ArtworkGUI.java
@@ -1,11 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ArtworkGUI {
-
+JFrame frame;
     //DECLARE VARIABLES
 
     public ArtworkGUI()
     {
+        frame = new JFrame("Artwork");
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new MyDrawing();
+        frame.add(panel);
+        frame.setVisible(true);
+
+        frame.setVisible(true);
         //CREATE THE GUI
     }
 
@@ -13,8 +24,31 @@ public class ArtworkGUI {
     {
         public MyDrawing(){
             //Set the background color
+            setBackground(Color.lightGray);
         }
+        public void paintComponent (Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.RED);
+            g.fillOval(100, 100, 200, 200);
+            g.setColor(Color.WHITE);
+            g.fillOval(125, 125, 150, 150);
+            g.setColor(Color.RED);
+            g.fillOval(150, 150, 100, 100);
+            g.setColor(Color.BLUE);
+            g.fillOval(175,175,50,50);
+            g.setColor(Color.WHITE);
+            g.drawLine(208,195,200,180);
+            g.drawLine(192,195,200,180);
+            g.drawLine(208,195,220,195);
+            g.drawLine(211,205,220,195);
+            g.drawLine(192,195,180,195);
+            g.drawLine(180,195,190,205);
+            g.drawLine(187,218,190,205);
+            g.drawLine(211,205,213,218);
+            g.drawLine(213,218,200,208);
+            g.drawLine(187,218,200,208);
 
-        //Add the PaintComponent Method and create your drawing.
-    }
+        }
+            //Add the PaintComponent Method and create your drawing.
+        }
 }
